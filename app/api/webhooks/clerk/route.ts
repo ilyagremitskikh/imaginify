64e2fac044ddd155fde00c465f67574c7e321cd9
Ilia Gremitskikh <gremilya@gmail.com>
2024-09-24 00:11:48 +0500

    clerk webhooks done

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
-// import { createUser } from '@/lib/actions/user.action';
+import { createUser } from '@/lib/actions/user.action';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
@@ -64,9 +64,9 @@ export async function POST(req: Request) {
       photo: image_url,
     };
 
-    // const newUser = await createUser(user);
+    const newUser = await createUser(user);
 
-    return NextResponse.json({ message: 'OK' });
+    return NextResponse.json({ message: 'OK', data: newUser });
   }
 
   return new Response('', { status: 200 });
